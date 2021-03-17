81fbff7ca0d6b295b7294b874df03fc74ee8e44a
Sha2h <47102008+Sha2h@users.noreply.github.com>
2021-03-17 20:37:01 +0530

    Add files via upload

---
+++ generated/source/buildConfig/release/com/ex/countdown/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.ex.countdown;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.ex.countdown";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
